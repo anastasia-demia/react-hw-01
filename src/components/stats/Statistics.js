ecd82c08241c15ef83a354fbffd7b4364f4758d1
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-05-10 17:45:40 +0300

    stats and css are in love

--- src/components/stats/Statistics.js
+++ src/components/stats/Statistics.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import css from "./stats.module.css"
 
 function randomHexColor() {
   return `#${Math.floor(Math.random() * 12345678).toString(16)}`;
@@ -8,20 +9,20 @@ const Stats = ({
   title, stats
 }) => {
   return (
-    <div class="box">
-      <section className="statistics">
-      <h2 class="title">{title}</h2>
+    <div class={css.box}>
+      <section className={css.statistics}>
+      <h2 class={css.title}>{title}</h2>
 
-      <ul className="stat-list">
+      <ul className={css.list}>
         {stats.map(({ id, label, percentage }) => {
           return (
             <li
-              className="stat-item"
+              className={css.item}
               key={id}
               style={{ backgroundColor: randomHexColor() }}
             >
-              <span className="label">{label}</span>
-              <span className="percentage">{percentage} %</span>
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage} %</span>
             </li>
           );
         })}
